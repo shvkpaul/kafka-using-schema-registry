2822ec990234082543d3970b859ef5caa283cb48
Shouvik Paul <shouvik.paul@ah.nl>
2024-02-02 15:28:02 +0100

    update swagger ui

--- src/main/kotlin/com/shvk/controller/EventController.kt
+++ src/main/kotlin/com/shvk/controller/EventController.kt
@@ -2,6 +2,7 @@ package com.shvk.controller
 
 import com.shvk.dto.Employee
 import com.shvk.producer.KafkaAvroProducer
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -12,6 +13,9 @@ class EventController(
     private val producer: KafkaAvroProducer
 ) {
 
+    @Operation(
+        summary = "Create a new event"
+    )
     @PostMapping("/events")
     fun sendMessage(@RequestBody employee: Employee): String {
         producer.send(employee)
