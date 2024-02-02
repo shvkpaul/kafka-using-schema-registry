c8a0b7417436eb68d6339a096d7f8cd98d624929
Shouvik Paul <shouvik.paul@ah.nl>
2024-02-02 12:14:10 +0100

    first commit

---
+++ src/main/kotlin/com/shvk/avro/KafkaUsingSchemaRegistryApplication.kt
@@ -0,0 +1,11 @@
+package com.shvk.avro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkaUsingSchemaRegistryApplication
+
+fun main(args: Array<String>) {
+	runApplication<KafkaUsingSchemaRegistryApplication>(*args)
+}
---
+++ src/test/kotlin/com/shvk/avro/KafkaUsingSchemaRegistryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shvk.avro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KafkaUsingSchemaRegistryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
